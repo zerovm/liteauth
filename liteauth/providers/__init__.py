5dfe260336d06097d3806cc6a8a2f11b6c7beb6e
Constantine Peresypkin <constantine.peresypk@rackspace.com>
2014-03-02 21:51:41 +0200

    bug in load_provider

--- liteauth/providers/__init__.py
+++ liteauth/providers/__init__.py
@@ -1,3 +1,4 @@
+
 def load_provider(name, klass, err_msg):
     try:
         mod = __import__('liteauth.providers.' + name, fromlist=[klass])
@@ -8,4 +9,4 @@ def load_provider(name, klass, err_msg):
 
 
 def load_oauth_provider(name):
-    load_provider(name, 'Client', 'oauth_provider is invalid in config file')
+    return load_provider(name, 'Client', 'oauth_provider is invalid in config file')
