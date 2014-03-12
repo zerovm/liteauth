0585dbeaff7bffd47d93b0d5701c4ca40b46e431
Constantine Peresypkin <constantine.peresypk@rackspace.com>
2014-03-12 19:42:50 +0200

    no easy way around CORS

--- liteauth/swauth_manager.py
+++ liteauth/swauth_manager.py
@@ -77,18 +77,31 @@ class SwauthManager(object):
 
     @wsgify
     def __call__(self, req):
-        resp = None
+        req_origin = req.headers.get('origin', None)
+        resp = self.handle(req)
+        if req_origin and req_origin in self.cors_allow_origin:
+                resp.headers['access-control-allow-origin'] = req_origin
+        return resp
+
+    def denied_response(self, req):
+        if req.remote_user:
+            self.logger.increment('forbidden')
+            return HTTPForbidden(request=req)
+        else:
+            self.logger.increment('unauthorized')
+            return HTTPUnauthorized(request=req)
+
+    def handle(self, req):
         if self.provider.is_disabled():  # profile management is disabled
-            resp = self.denied_response(req)
-        endpoint = None
+            return self.denied_response(req)
         try:
             (endpoint, _rest) = req.split_path(1, 2, True)
         except ValueError:
-            resp = self.denied_response(req)
-        if endpoint and endpoint == self.profile_path:
+            return self.denied_response(req)
+        if endpoint == self.profile_path:
             account_id = req.environ.get('REMOTE_USER', '')
             if not account_id:
-                resp = HTTPUnauthorized(request=req)
+                return HTTPUnauthorized(request=req)
             user_id, user_email = account_id.split(':')
             whitelist_data = get_data_from_url(self.whitelist_url,
                                                self.app,
@@ -103,13 +116,13 @@ class SwauthManager(object):
                                                 self.logger,
                                                 req.environ)
                 if not invite_data or isinstance(invite_data, dict):
-                    resp = self.denied_response(req)
+                    return self.denied_response(req)
                 try:
                     invite_data = json.loads(invite_data)
                 except Exception:
-                    resp = HTTPInternalServerError(request=req)
+                    return HTTPInternalServerError(request=req)
                 if not invite_data.get('service', None):
-                    resp = HTTPInternalServerError(request=req)
+                    return HTTPInternalServerError(request=req)
                 if not invite_data.get('email', None):
                     invite_data['email'] = user_email
                     invite_data['user_id'] = user_id
@@ -118,31 +131,31 @@ class SwauthManager(object):
                                              invite_code,
                                              json.dumps(invite_data),
                                              req.environ):
-                        resp = HTTPInternalServerError(request=req)
+                        return HTTPInternalServerError(request=req)
                     if not store_data_in_url(self.whitelist_url,
                                              self.app,
                                              user_email,
                                              json.dumps(invite_data),
                                              req.environ):
-                        resp = HTTPInternalServerError(request=req)
+                        return HTTPInternalServerError(request=req)
                 elif not whitelist_data and invite_data['email'] == user_email:
                     if not store_data_in_url(self.whitelist_url,
                                              self.app,
                                              user_email,
                                              json.dumps(invite_data),
                                              req.environ):
-                        resp = HTTPInternalServerError(request=req)
+                        return HTTPInternalServerError(request=req)
             else:
                 if whitelist_data:
                     try:
                         whitelist_data = json.loads(whitelist_data)
                     except Exception:
-                        resp = HTTPInternalServerError(request=req)
+                        return HTTPInternalServerError(request=req)
                 if not whitelist_data or not isinstance(whitelist_data, dict):
-                    resp = Response(request=req, status=402,
+                    return Response(request=req, status=402,
                                     body='Account not in whitelist')
                 if not whitelist_data.get('service', None):
-                    resp = HTTPInternalServerError(request=req)
+                    return HTTPInternalServerError(request=req)
                 current_user = whitelist_data.get('user_id', None)
                 if not current_user:
                     whitelist_data['email'] = user_email
@@ -152,36 +165,22 @@ class SwauthManager(object):
                                              user_email,
                                              json.dumps(whitelist_data),
                                              req.environ):
-                        resp = HTTPInternalServerError(request=req)
+                        return HTTPInternalServerError(request=req)
                 elif current_user != user_id:
                     # user subscribed to the service already
                     # but used a different auth provider
-                    resp = HTTPConflict(request=req)
+                    return HTTPConflict(request=req)
             if req.method == 'GET':
-                resp = self.provider.get_user(self.app,
+                return self.provider.get_user(self.app,
                                               req,
                                               user_id,
                                               user_email)
             elif req.method == 'PUT':
-                resp = self.provider.put_user(self.app,
+                return self.provider.put_user(self.app,
                                               req,
                                               user_id,
                                               user_email)
-        if not resp:
-            resp = self.denied_response(req)
-        req_origin = req.headers.get('origin', None)
-        if req_origin and req_origin in self.cors_allow_origin:
-                resp.headers['access-control-allow-origin'] = req_origin
-        return resp
-
-    def denied_response(self, req):
-        if req.remote_user:
-            self.logger.increment('forbidden')
-            return HTTPForbidden(request=req)
-        else:
-            self.logger.increment('unauthorized')
-            return HTTPUnauthorized(request=req)
-
+        return self.denied_response(req)
 
 def filter_factory(global_conf, **local_conf):
     """Returns a WSGI filter app for use with paste.deploy."""
